e67355c08c5cac82531e31f565ab43ddfde90cdb
T.J. Cotta <TJCottaSE@gmail.com>
2017-11-12 22:14:33 -0800

    #9 first commit, could need testing

--- SER421-Lab4/Clue.js
+++ SER421-Lab4/Clue.js
@@ -8,6 +8,7 @@
 var suspects = ['Miss Scarlet', 'Colonel Mustard', 'Mrs. White', 'Mr. Green', 'Professor Plum'];
 var weapons = ['Candlestick', 'Dagger', 'Lead Pipe', 'Revolver', 'Rope', 'Wrench'];
 var rooms = ['Kitchen', 'Ballroom', 'Conservatory', 'Billiard Room', 'Library', 'Study', 'Hall', 'Lounge', 'Dining Room'];
+var playerTurn = 'true';
 var playerCards = [];
 var computerCards = [];
 var solution = [/* Suspect, Weapon, Room */];
@@ -19,7 +20,8 @@ var solution = [/* Suspect, Weapon, Room */];
 function dispUser(){
     var x = document.forms['player'];
     var name = x.elements[0].value;
-    console.log('User name is: ' + name);
+	//console.log('User name is: ' + name);
+	sessionStorage.setItem('playerName', name);
     document.getElementById('userGreeting').innerHTML = 'Welcome ' + name;
 }
 
@@ -112,7 +114,7 @@ function checkGuess(){
 	var suspect = x.elements[0].value;
 	var weapon = x.elements[1].value;
 	var room = x.elements[2].value;
-	console.log("Guess is: " + suspect + ", " + weapon + ", " + room);
+	//console.log("Guess is: " + suspect + ", " + weapon + ", " + room);
 	// Log guess to session local storage
 	var thisGuess = [suspect, weapon, room];
 	var allGuesses = JSON.parse(sessionStorage.getItem("allGuesses"));
@@ -132,9 +134,10 @@ function checkGuess(){
 		solution.includes(room)){
 			document.getElementById('result').innerHTML = 'Winner!!!!! That was the correct guess.';
 			// Display restart button
-			console.log("WINNNER!!!!!!!");
+			//console.log("WINNNER!!!!!!!");
 			removeElement('btn');
 			addElement('continue', 'button', 'btn', 'onclick', 'restartGame()', 'Restart');
+			setWinner();
 			// Should remove submit button from drop downs to force player to let computer play
 			// Also means it needs to be re-added after computer plays
 			// IMPLEMENT THIS
@@ -201,7 +204,7 @@ function showHistory(){
 	var currentText = document.getElementById('history').innerHTML;
 	if (currentText == 'Show History'){
 		// print the guess history
-		console.log('show the history');
+		//console.log('show the history');
 		document.getElementById('history').innerHTML = 'Hide History';
 		var allGuesses = JSON.parse(sessionStorage.getItem("allGuesses"));
 		var node = document.createElement('p'); 
@@ -216,18 +219,63 @@ function showHistory(){
 		document.getElementById('guessHistory').appendChild(node);
 	}
 	else {
-		console.log('Hide the history');
+		//console.log('Hide the history');
 		document.getElementById('history').innerHTML = 'Show History';
 		document.getElementById('guessHistory').innerHTML = '';
 		// ^^ THis may not work ^^
 	}
-	// var node = document.createElement('p'); 
-	// var textnode = document.createTextNode('Proposed: ' + 
-	// 	suspect + ' killed Dr. Black with a ' + weapon + ' in the ' + room +'.');
-	// node.appendChild(textnode);
-	// document.getElementById('guessHistory').appendChild(node);
 }
 
+/*
+*
+*  [ date, who, result ]
+*/
+function showRecord(){
+	//var hist = ['10/25/17', 'TJ', 'lost', '10/29/17', 'Karen', 'won'];
+	//localStorage.setItem('gameHistories', JSON.stringify(hist));
+	// ^^ Test Code ^^
+	var history = JSON.parse(localStorage.getItem('gameHistories'));
+	var node = document.createElement('p');
+	for (i = 0; i < history.length; i+=3){
+		var textnode = document.createTextNode(history[i] + 
+			': ' + history[i+1] + ' ' + history[i+2] + '.');
+		node.appendChild(textnode);
+		node.appendChild(document.createElement('br'));
+	}
+	document.getElementById('gameRecords').appendChild(node);
+}
+
+// Function stores the win/loss record to localStorage
+function setWinner(){
+	var pName = sessionStorage.getItem('playerName');
+	var date = new Date().toDateString();
+	var result = '';
+	if (playerTurn == 'true'){
+		result = 'won';
+	}
+	else {
+		result = 'lost';
+	}
+	if (pName){
+		var h = JSON.parse(localStorage.getItem('gameHistories'));
+		if (h) {
+			h.push(date, pName, result);
+		}
+		else {
+			h = [date, pName, result];
+		}
+	}
+	else {
+		var h = JSON.parse(localStorage.getItem('gameHistories'));
+		if (h){
+			h.push(date, 'Guest', result);
+		}
+		else {
+			h = [date, 'Guest', result];
+		}
+	}
+	localStorage.setItem('gameHistories', JSON.stringify(h));
+}
 /*
 * Creates the list of cards able to be 
 shown on the list for display
@@ -273,9 +321,9 @@ function populateRooms(){
 var shuffledSuspects = shuffle(suspects);
 var shuffledWeapons = shuffle(weapons);
 var shuffledRooms = shuffle(rooms)
-console.log(shuffledSuspects);
-console.log(shuffledWeapons);
-console.log(shuffledRooms);
+//console.log(shuffledSuspects);
+//console.log(shuffledWeapons);
+//console.log(shuffledRooms);
 
 // Test the pick winner function
 solution = pickWinningCards(shuffledSuspects, shuffledWeapons, shuffledRooms);
@@ -285,13 +333,13 @@ console.log('The winning combo is ' + solution);
 var playableSuspects =  stripOutSolution(suspects, solution);
 var playableWeapons =  stripOutSolution(weapons, solution);
 var playableRooms = stripOutSolution(rooms, solution);
-console.log('Playable Suspects: ' + playableSuspects);
-console.log('Playable Weapons: ' + playableWeapons);
-console.log('Playable Rooms: ' + playableRooms);
+//console.log('Playable Suspects: ' + playableSuspects);
+//console.log('Playable Weapons: ' + playableWeapons);
+//console.log('Playable Rooms: ' + playableRooms);
 
 // Test Dealing cards
 var remainingCards = playableSuspects.concat(playableRooms, playableWeapons);
-console.log('All cards shuffled: ' + remainingCards);
+//console.log('All cards shuffled: ' + remainingCards);
 
 
 // Shuffle and deal the remaining cards
@@ -301,5 +349,6 @@ computerCards = deltCards[1];
 // ^^ This is business logic and not testing code ^^
 
 
-console.log('Player Cards: ' + playerCards);
-console.log('Computer Cards: ' + computerCards);
+//console.log('Player Cards: ' + playerCards);
+//console.log('Computer Cards: ' + computerCards);
+
